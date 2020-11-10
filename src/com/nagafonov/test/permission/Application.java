7eedb370445238cea3dee128935a0d1cd828cc27
Agafonov Nikita <agns61.1@gmail.com>
2020-11-10 18:13:46 +0300

    set contructors; read field from console;

--- src/com/nagafonov/test/permission/Application.java
+++ src/com/nagafonov/test/permission/Application.java
@@ -1,8 +1,43 @@
 package com.nagafonov.test.permission;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @Author Agafonov Nikita agns61.1@gmail.com
  * @create 11/9/2020 7:29 PM
  */
 public class Application {
+
+    public static void main(String[] args) {
+        CheckPermission checkPermission;
+        System.out.println("Если Вы хотите проверить водителя по номеру ТС - нажмите 1, по номеру разрешения - нажмите 2");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int typeOfCheck = Integer.parseInt(bufferedReader.readLine());
+            switch (typeOfCheck) {
+                case 1:
+                    System.out.println("Введите номер ТС (в формате С000СС или СС000):");
+                    String autoNumber = bufferedReader.readLine();
+
+                    System.out.println("Введите регионт ТС (например, 16, 116 или 716):");
+                    String autoCity = bufferedReader.readLine();
+                    checkPermission = new CheckPermission(autoNumber, autoCity);
+                    break;
+
+                case 2:
+                    System.out.println("Введите номер разрешения:");
+                    int numberPermission = Integer.parseInt(bufferedReader.readLine());
+                    checkPermission = new CheckPermission(numberPermission);
+                    break;
+
+                default:
+                    System.out.println("Неверный ввод!");
+                    System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/nagafonov/test/permission/CheckPermission.java
+++ src/com/nagafonov/test/permission/CheckPermission.java
@@ -4,9 +4,19 @@ package com.nagafonov.test.permission;
  * @Author Agafonov Nikita agns61.1@gmail.com
  * @create 11/10/2020 5:42 PM
  */
-public class CheckParsePermission {
-    private String contentGet;
+public class CheckPermission {
+    private String autoNumber;
+    private String autoCity;
+    private int numberPermission;
 
+    public CheckPermission(String autoNumber, String autoCity) {
+        this.autoNumber = autoNumber;
+        this.autoCity = autoCity;
+    }
+
+    public CheckPermission(int numberPermission) {
+        this.numberPermission = numberPermission;
+    }
 
     // true if permission exists
     private boolean checkPermission(String contentGet) {
